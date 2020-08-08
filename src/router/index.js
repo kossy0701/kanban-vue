8fb2ebb7ef59cc4e9a93a7046646adddd27d110b
kossy0701 <0421kossy0421@gmail.com>
2020-08-08 10:35:42 +0900

    define tasks path

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,31 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import KbnLoginView from "../components/templates/KbnLoginView.vue";
+import KbnLoginView from "@/components/templates/KbnLoginView.vue";
+import KbnBoardView from "@/components/templates/KbnBoardView.vue";
+import KbnTaskDetailModal from "@/components/templates/KbnTaskDetailModal.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
+    path: "/",
+    component: KbnBoardView,
+    children: [
+      {
+        path: "tasks/:id",
+        component: KbnTaskDetailModal,
+        name: "taskDetailModal"
+      }
+    ]
+  },
+  {
     path: "/login",
     name: "Login",
     component: KbnLoginView
+  },
+  {
+    path: "*",
+    redirect: "/"
   }
 ];
 
